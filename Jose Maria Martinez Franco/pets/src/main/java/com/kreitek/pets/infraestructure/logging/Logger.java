54452aa48d2b62d31648669fa8e607af054618b7
chemamtnz <163119030+chemamtnz@users.noreply.github.com>
2024-03-13 17:19:36 +0100

    Add files via upload

--- Jose Maria Martinez Franco/pets/src/main/java/com/kreitek/pets/infraestructure/logging/Logger.java
+++ Jose Maria Martinez Franco/pets/src/main/java/com/kreitek/pets/infraestructure/logging/Logger.java
@@ -1,21 +1,23 @@
 package com.kreitek.pets.infraestructure.logging;
 
 public class Logger {
-    private static Logger instance;
-    private static int counter = 0;
+    private static class SingletonHolder {
+        private static final Logger INSTANCE = new Logger();
+    }
+
+    private int counter = 0;
 
     private Logger() {
     }
 
-    public static synchronized Logger getInstance() {
-        if (instance == null) {
-            instance = new Logger();
-        }
-        return instance;
+    public static Logger getInstance() {
+        return SingletonHolder.INSTANCE;
     }
 
     public void debug(String message) {
-        counter++;
+        synchronized (this) {
+            counter++;
+        }
         System.out.println("[debug][" + counter + "] - " + message);
     }
 
