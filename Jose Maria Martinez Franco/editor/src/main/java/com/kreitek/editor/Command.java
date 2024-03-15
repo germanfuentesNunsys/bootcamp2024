a1e1d76d508ae391506c27e0baae89eb59196d98
chemamtnz <163119030+chemamtnz@users.noreply.github.com>
2024-03-15 13:20:52 +0100

    Delete Jose Maria Martinez Franco/editor directory

--- Jose Maria Martinez Franco/editor/src/main/java/com/kreitek/editor/Command.java
+++
@@ -1,8 +0,0 @@
-package com.kreitek.editor;
-
-import java.util.ArrayList;
-
-public interface Command {
-    void execute(ArrayList<String> documentLines);
-    void undo(ArrayList<String> documentLines);
-}
